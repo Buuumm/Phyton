426f567fa182bed63d2d00b850589247d0f1153f
DanSZ <buuumm96@gmail.com>
2019-11-14 17:47:49 +0100

    Add files via upload

---
+++ Cw1/cw6+.py
@@ -0,0 +1,38 @@
+#6
+lista1=[1,2,3,4,5,6,7,8,9,10]
+lista2=lista1[5:10]
+lista1=[1,2,3,4,5]
+
+print(lista1)
+print(lista2)
+
+#7
+lista3=lista1+lista2
+print(lista3)
+lista3.insert(0,0)
+print(lista3)
+
+#8
+a=(1, 'Stefan', 'Stefanowicz')
+b=(2, 'Stefanow', 'Zenonowicz')
+c=(3, 'Zenon', 'Zenonowski')
+print(a[0:])
+
+#9
+a_slow = dict(index=1, imie='Stefan', nazwisko='Stefanowicz', wiek=20, email='a@a.pl',
+              rok_ur=1998, adres='afsd')
+b_slow = dict(index=2, imie='Stefan2', nazwisko='Stefanowicz2', wiek=20, email='b@b.pl',
+              rok_ur=1998, adres='afsdafasdfas')
+c_slow = dict(index=3, imie='Stefan3', nazwisko='Stefanowicz3', wiek=20, email='b@b.pl',
+              rok_ur=1998, adres='afsdafas')
+
+#13
+jakieslowniki = [{"cos": 1, "cos2": "costam"}, {"cos3": 3, "cos4": 12123310}, {"cos5": 6, 6: "asdfasdfasdfasdfasdf"}]
+napis = ""
+for x in jakieslowniki:
+    for y in x.keys():
+        napis += str(x[y]) + " "
+print(napis)
+
+
+
